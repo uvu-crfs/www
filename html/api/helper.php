fafc071af1395abf10ddf728fea38a240215f80e
Jason Mavandi <mvndaai@gmail.com>
2016-10-17 18:40:06 -0600

    Read from a hidden file

--- html/api/helper.php
+++ html/api/helper.php
@@ -1,3 +1,11 @@
 <?php
 echo readfile("test.txt");
+
+$string = file_get_contents("/var/www/lib/database.json");
+$json_a=json_decode($string,true);
+
+foreach ($json_a as $key => $value){
+  echo  "\r\n" . $key . ':' . $value;
+}
+
 ?>
