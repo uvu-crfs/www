d0f64b05265d2d575c1a985166f46913c17f5cb9
Jason Mavandi <mvndaai@gmail.com>
2016-10-20 22:56:51 -0600

    Start PDO

---
+++ html/html/index.php
@@ -0,0 +1,12 @@
+<html>
+
+<head></head>
+
+<body>
+  Welcome to our site.
+  <br>
+  <a href="phpinfo.php">PHP info page</a>
+  <br><a href="api/helper.php">helper page</a>
+</body>
+
+</html>
--- lib/database.php
+++ lib/database.php
@@ -1,12 +1,17 @@
 <?php
-
+    //PDO tutorial https://phpdelusions.net/pdo
     $db_json = file_get_contents('/var/www/lib/database.json');
     $db = json_decode($db_json, true);
 
     $servername = $db['host'].':'.$db['port'];
     echo "Server Name: $servername <br>";
 
-    echo 'PDO_MYSQL - '.(int) extension_loaded('PDO_MYSQL');
-    echo '<br>mysqli - '.(int) extension_loaded('mysqli');
-    echo '<br><br><br>';
-    echo var_dump(get_loaded_extensions());
+    $dsn = "mysql:host=$servername;dbname=".$db['dbname'].';charset=utf8';
+    $opt = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    //Something is broken on the next line
+    $pdo = new PDO($dsn, $db['username'], $db['password'], $opt);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
