1f532a556794a26a05a0fa12cdac2c3c1a9da69e
Jason Mavandi <mvndaai@gmail.com>
2016-11-15 20:24:17 -0700

    Cleanup file

--- lib/helper_functions.php
+++ lib/helper_functions.php
@@ -10,15 +10,17 @@
       header('Content-Type: application/json');
       echo json_encode($array);
   }
+
   function post_data()
   {
       return json_decode(file_get_contents('php://input'), true);
   }
 
-  function dump_array($array)
+  function print_array($array)
   {
       var_dump($array);
   }
+
   function whole_int($val)
   {
       //http://php.net/manual/en/function.is-numeric.php
