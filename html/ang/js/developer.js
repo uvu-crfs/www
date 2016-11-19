e2fa3304e8aba4b75a462533c777af1862581ce2
Jason Mavandi <Jason.Mavandi@emc.com>
2016-11-19 16:01:20 -0700

    Developer controller

--- html/ang/js/developer.js
+++ html/ang/js/developer.js
@@ -1,1 +1,16 @@
 console.log("You must be a developer");
+
+var crfsApp = angular.module('crfsApp', ['ngRoute']);
+
+crfsApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/developer', {
+            templateUrl : '/ang/pages/developer.html',
+            controller  : 'developerController'
+        })
+    ;
+});
+
+crfsApp.controller('developerController', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
