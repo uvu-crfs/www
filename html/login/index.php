031fb3d948e1dd54c0ba253ec619590a2e9db09f
Jason Mavandi <Jason.Mavandi@emc.com>
2017-01-30 13:58:07 -0700

    .

--- html/login/index.php
+++ html/login/index.php
@@ -3,6 +3,10 @@
 echo 'displayName: '.getenv('displayName').'<br>';
 echo 'mail: '.getenv('mail').'<br>';
 echo 'uvuEntitlements: '.getenv('uvuEntitlements').'<br>';
+echo '<br>';
+echo 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'."{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}";
+echo print_r($_SERVER);
+
 ?>
 
 <a href="https://cas.uvu.edu/cas/logout?destination=url">Logout</a>
