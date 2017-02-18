26aca43b62ad9dd56bb700532c1c00d298dda7d5
Erick Diaz <ediaz@spark.net>
2017-02-18 15:16:48 -0700

    Updating the deploy a bit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR erik@stu-web4.uvu.edu:/var
-ssh erik@stu-web4.uvu.edu 'cp ~/database.json /var/www/lib/'
+#ssh erik@stu-web4.uvu.edu 'cp ~/database.json /var/www/lib/'
