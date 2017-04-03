9257126779c819e8fc10ef04b6bf8b80556772b9
Jason Mavandi <Jason.Mavandi@emc.com>
2017-04-03 17:12:06 -0600

    Complete leaderboard phase 1

---
+++ html/mithril/components/leaderboards.js
@@ -0,0 +1,56 @@
+var leaderboard = {
+  //oninit:(vnode)=> console.log(vnode.attrs.name, vnode.attrs),
+  view:(vnode)=> m('',[
+    m(`#leaderboard${vnode.attrs.id}`,JSON.stringify(vnode.attrs)),
+  ]),
+  oncreate:(vnode) => {
+    let cols = [['x']];
+    vnode.attrs.data.forEach((e,idx) => {
+      cols[0].push(e.group_name);
+      let row = [e.group_name];
+      for(var i=0; i < idx; i++){ row.push(null); }
+      row.push(e.total);
+      cols.push(row);
+    });
+    c3.generate({
+      bindto: `#leaderboard${vnode.attrs.id}`,
+      data: {
+        x : 'x',
+        columns: cols,
+        type: 'bar',
+        groups : [vnode.attrs.data.map((e) => e.group_name)],
+      },
+      legend: { hide: true },
+      axis: { x: { type: 'category' },  y: { label: vnode.attrs.unit }, },
+      title: { text: `${vnode.attrs.name}` },
+      bar: { width: { ratio: 0.5 } }
+    });
+  }
+};
+
+
+export default {
+  leaderboards:{},
+  getLeaderboard:(vnode, sensor_info) => {
+    //TODO limit to date range using &start=<dateTimestamp>&end=<dateTimestamp>,
+    m.request(`/api/open/leaderboard.php?sensor_id=${sensor_info.id}`).then(
+      (r) => {
+        if (r.length === 0) return;
+        vnode.state.leaderboards[sensor_info.id] = sensor_info;
+        vnode.state.leaderboards[sensor_info.id].data = r;
+      },
+      (e) => console.log(e)
+    );
+  },
+  oninit:(vnode) => {
+    m.request('/api/open/sensor/types.php').then(
+      (r) => r.forEach((s)=> vnode.state.getLeaderboard(vnode, s)),
+      (e) => console.log(e)
+    );
+  },
+  view:(vnode) => m('',[
+    m('.title', `Leaderboard${Object.keys(vnode.state.leaderboards).length>1?'s':''}`),
+    Object.keys(vnode.state.leaderboards).map((l) => m(leaderboard, vnode.state.leaderboards[l])),
+  ]),
+
+};
--- html/mithril/views/home.js
+++ html/mithril/views/home.js
@@ -1,5 +1,6 @@
 import {getSensors} from '/mithril/utils.js';
-import AddSensorData from '/mithril/components/addSensorData.js';
+import addSensorData from '/mithril/components/addSensorData.js';
+import leaderboards from '/mithril/components/leaderboards.js';
 
 export default {
   oninit: function(vnode){
@@ -13,11 +14,12 @@ export default {
   view: function(vnode) {
     return m('',[
       m('.title','Welcome Home'),
+      g.uvu.loggedIn && vnode.state.activeVisits.length > 0 ? m('',
+        g.sensors.map(function(v){ return m(addSensorData,{sensor:v, visits: vnode.state.activeVisits}); }))
+      : null,
+      m(leaderboards),
       m("a[href='http://www.uvu.edu/crfs/']",
         'Capitol Reef Field Station Home'),
-      g.uvu.loggedIn && vnode.state.activeVisits.length > 0 ? m('',
-        g.sensors.map(function(v){ return m(AddSensorData,{sensor:v, visits: vnode.state.activeVisits}); }))
-      : null,
     ]);
   }
 };
