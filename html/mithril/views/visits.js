7b909624467ecfecc3480fbb6c90bd4a8c51dd91
Mitchell Williams <meotch_williams@yahoo.com>
2017-04-15 10:44:39 -0600

    editModal popup working
    Still needs to be loaded w/ data

--- html/mithril/views/visits.js
+++ html/mithril/views/visits.js
@@ -151,7 +151,7 @@ let editVisitModalBody = (vnode) => [
 
 let visitCard = {
   oninit:(vnode) => {
-    vnode.state.edit = {modal:false, type: 'visit', func:getVisit, body:editVisitModalBody};
+    // vnode.state.edit = {modal:false, type: 'visit', func:getVisit, body:editVisitModalBody};
     vnode.state.open = false;
     vnode.state.values = [];
     vnode.state.openDetails = _ => {
@@ -165,22 +165,22 @@ let visitCard = {
       m('.fa', {class:vnode.state.open?'fa-angle-down':'fa-angle-up'}, ''),
     ]),
     m('span', {style:'font-weight: bold;'},`Start:`),
-    m('span', {style:'padding:0 5px;'},`${unixToDate(vnode.attrs.start_date)}`),
+    m('span', {style:'padding:0 5px;'},`${unixToDate(vnode.attrs.visit.start_date)}`),
     m('span', {style:'font-weight: bold;'},`End:`),
-     m('span', {style:'padding:0 5px;'},`${unixToDate(vnode.attrs.end_date)}`),
-    g.groupLookup[vnode.attrs.group_id] ? m('span', {style:"font-weight:bold;"}, g.groupLookup[vnode.attrs.group_id].name) : null,
+     m('span', {style:'padding:0 5px;'},`${unixToDate(vnode.attrs.visit.end_date)}`),
+    g.groupLookup[vnode.attrs.visit.group_id] ? m('span', {style:"font-weight:bold;"}, g.groupLookup[vnode.attrs.visit.group_id].name) : null,
     //m('span', vnode.attrs.name),
     vnode.state.open ? m('button.button.is-danger.is-small.is-pulled-right',
       {onclick:_ => {
         deleteData.modal = true;
-        deleteData.id = vnode.attrs.id;
-        deleteData.name = `${vnode.attrs.name}`;
+        deleteData.id = vnode.attrs.visit.id;
+        deleteData.name = `${vnode.attrs.visit.name}`;
       }}, 'Delete') : null,
     vnode.state.open ? m('button.button.is-primary.is-small.is-pulled-right',
       {onclick:_ => {
-        //TODO display Edit Modal filled w/ values
-        vnode.state.edit.modal = true;
-        console.log(`Value of vnode.edit.modal ${vnode.state.edit.modal}`);
+        //TODO display Edit Modal filled w/ value
+        vnode.attrs.editModal.modal = true;
+
       }}, 'Edit') : null,
     vnode.state.open ? m('',[
       m('', JSON.stringify(vnode.attrs)),
@@ -191,7 +191,7 @@ let visitCard = {
 
 export default {
   oninit:(vnode) => {
-    vnode.state.add = {modal:false, type: 'sensor', func:addVisit, body:addVisitModalBody };
+    vnode.state.add = {modal:false, type: 'visit', func:addVisit, body:addVisitModalBody };
     vnode.state.edit = {modal:false, type: 'visit', func:getVisit, body:editVisitModalBody};//must be passed to each visitCard so editModal can be triggered
     if (g.visits.length === 0) getVisits();
     if (g.groups.length === 0) getGroups();
@@ -202,7 +202,7 @@ export default {
       m('.level-right',m('button.button.is-primary.add-button', {onclick:function(){ vnode.state.add.modal = true;}} ,'Add')),
     ]),
     m(addVisitModal, vnode.state.add),
-    m(editVisitModal, vnode.state.edit),//TODO verify
-    g.visits.map((v)=> m(visitCard, v)),
+    m(editVisitModal, vnode.state.edit),
+    g.visits.map((v)=> m(visitCard, {visit:v,editModal:vnode.state.edit})),//becomes vnode.attrs of visitCard
   ])
 };
