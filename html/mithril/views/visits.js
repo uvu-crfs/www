d40939699d47dafd4ed4c01de7e0b89e07558427
Mitchell Williams <meotch_williams@yahoo.com>
2017-04-15 16:01:45 -0600

    Edit Visit Modal updated
    add visit lingering data bug resolved

--- html/mithril/views/visits.js
+++ html/mithril/views/visits.js
@@ -1,7 +1,7 @@
-import {getGroups, getVisit, getVisits, htmlDateToUnix, addVisit, unixToDate, blankFirstOption, getTimeStamp} from '/mithril/utils.js';
+import {getGroups, getVisit, getVisits, editVisit, htmlDateToUnix, addVisit, unixToDate, blankFirstOption, getTimeStamp} from '/mithril/utils.js';
 import {addVisitModal, editVisitModal, deleteModal} from '/mithril/components/modals.js';
 import {s2Component} from '/mithril/components/s2Component.js';
-//let deleteData = {modal:false, type:'sensor', func: deleteVisit};
+// let deleteData = {modal:false, type:'visit', func: deleteVisit};
 
 // var makeAutoComplete = function(element, isInitialized) {
 //   let groupNames = g.groups.map((v) => v.name);
@@ -16,12 +16,6 @@ import {s2Component} from '/mithril/components/s2Component.js';
 //let groupNames = g.groups.map((v) => {id: v.id}, {name: v.name});
 
 let addVisitModalBody = (vnode) => [
-  vnode.state.evaluation_checked = false,
-  vnode.state.summary_checked = false,
-  vnode.state.darksky_checked = false,
-  vnode.state.data.evaluation = vnode.state.evaluation_checked,
-  vnode.state.data.summary = vnode.state.summary_checked,
-  vnode.state.data.darksky = vnode.state.darksky_checked,
   m('.label', 'Group'),
   m(s2Component, {
     request_data: vnode.state.data,
@@ -39,37 +33,34 @@ let addVisitModalBody = (vnode) => [
   m('.label', 'End Date'),
   m('input[type="date"]', {oninput:(e) => vnode.state.data.end_date = htmlDateToUnix(e.target.value) }, ''),
   m('.label', 'Days'),
-  m('input.input', {onchange:(e) => vnode.state.data.days = e.target.value }, ''),
+  m('input.input[type="number"]', {onchange:(e) => vnode.state.data.days = e.target.value }, ''),
   m('.label', 'Nights'),
-  m('input.input', {onchange:(e) => vnode.state.data.nights = e.target.value }, ''),
+  m('input.input[type="number"]', {onchange:(e) => vnode.state.data.nights = e.target.value }, ''),
   m('.label', 'Female Students'),
-  m('input.input', {onchange:(e) => vnode.state.data.students_female = e.target.value }, ''),
+  m('input.input[type="number"]', {onchange:(e) => vnode.state.data.students_female = e.target.value }, ''),
   m('.label', 'Male Students'),
-  m('input.input', {onchange:(e) => vnode.state.data.students_male = e.target.value }, ''),
+  m('input.input[type="number"]', {onchange:(e) => vnode.state.data.students_male = e.target.value }, ''),
   m('.label', 'Advisors'),
-  m('input.input', {onchange:(e) => vnode.state.data.advisors = e.target.value }, ''),
+  m('input.input[type="number"]', {onchange:(e) => vnode.state.data.advisors = e.target.value }, ''),
   m('.label', 'Evaluation Complete'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
-      onclick: _ => {
-        vnode.state.evaluation_checked = !vnode.state.evaluation_checked;
-        vnode.state.data.evaluation = vnode.state.evaluation_checked;
+    m('input[type="checkbox"]', {
+      onclick: (e) => {
+        vnode.state.data.evaluation =  e.target.checked;
       }
     }, ''), ''),
   m('.label', 'Summary Complete'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
-      onclick: _ => {
-        vnode.state.summary_checked = !vnode.state.summary_checked;
-        vnode.state.data.summary = vnode.state.summary_checked;
+    m('input[type="checkbox"]', {
+      onclick: (e) => {
+        vnode.state.data.summary = e.target.checked;
       }
     }, ''), ''),
   m('.label', 'Dark Sky Tour Given'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
-      onclick: _ => {
-        vnode.state.darksky_checked = !vnode.state.darksky_checked;
-        vnode.state.data.darksky = vnode.state.darksky_checked;
+    m('input[type="checkbox"]', {
+      onclick: (e) => {
+        vnode.state.data.darksky = e.target.checked;
       }
     }, ''), ''),
   m('.label', 'Notes'),
@@ -83,6 +74,7 @@ let editVisitModalBody = (vnode) => [
   m(s2Component, {
     request_data: vnode.state.data,
     request_attr: 'group_id',
+    value: vnode.attrs.data.group,
     data: g.groups.map(function(obj) {
       let newObj = {};
       newObj.id = obj.id;
@@ -90,52 +82,75 @@ let editVisitModalBody = (vnode) => [
       return newObj;
     })
   }),
-  //m('button', {disabled:true}, 'Add') //TODO this should add a group
   m('.label', 'Start Date'),
-  m('input[type="date"]', {oninput:(e) => {vnode.state.data.start_date = htmlDateToUnix(e.target.value); console.log(vnode.state.data); }}, vnode.state.data.start_date),//TODO was ''
+  m('input[type="date"]', {
+    date: vnode.attrs.data.start_date,
+    oninput:(e) => vnode.state.data.start_date = htmlDateToUnix(e.target.value)
+    }, ''),
   m('.label', 'End Date'),
-  m('input[type="date"]', {oninput:(e) => vnode.state.data.end_date = htmlDateToUnix(e.target.value) }, ''),
+  m('input[type="date"]', {
+    value: vnode.attrs.data.end_date,
+    oninput:(e) => vnode.state.data.end_date = htmlDateToUnix(e.target.value)
+  }, ''),
   m('.label', 'Days'),
-  m('input.input[type=number]', {
+  m('input.input[type="number"]', {
     value: vnode.attrs.data.days,
     onchange:(e) => vnode.attrs.data.days = e.target.value
   }, ''),
   m('.label', 'Nights'),
-  m('input.input', {onchange:(e) => vnode.state.data.nights = e.target.value }, ''),
+  m('input.input[type="number"]', {
+    value: vnode.attrs.data.nights,
+    onchange:(e) => vnode.state.data.nights = e.target.value
+  }, ''),
   m('.label', 'Female Students'),
-  m('input.input', {onchange:(e) => vnode.state.data.students_female = e.target.value }, ''),
+  m('input.input[type="number"]', {
+    value: vnode.attrs.data.students_female,
+    onchange:(e) => vnode.state.data.students_female = e.target.value
+  }, ''),
   m('.label', 'Male Students'),
-  m('input.input', {onchange:(e) => vnode.state.data.students_male = e.target.value }, ''),
+  m('input.input[type="number"]', {
+    value: vnode.attrs.data.students_male,
+    onchange:(e) => vnode.state.data.students_male = e.target.value
+  }, ''),
   m('.label', 'Advisors'),
-  m('input.input', {onchange:(e) => vnode.state.data.advisors = e.target.value }, ''),
+  m('input.input', {
+    value: vnode.attrs.data.advisors,
+    onchange:(e) => vnode.state.data.advisors = e.target.value
+  }, ''),
   m('.label', 'Evaluation Complete'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
+    m('input[type="checkbox"]', {
+      checked: vnode.attrs.data.evaluation,
       onclick: _ => {
-        vnode.state.evaluation_checked = !vnode.state.evaluation_checked;
-        vnode.state.data.evaluation = vnode.state.evaluation_checked;
+        vnode.attrs.data.evaluation = !vnode.attrs.data.evaluation;
       }
     }, ''), ''),
   m('.label', 'Summary Complete'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
-      onclick: _ => {
-        vnode.state.summary_checked = !vnode.state.summary_checked;
-        vnode.state.data.summary = vnode.state.summary_checked;
+    m('input[type="checkbox"]', {
+      checked: vnode.attrs.data.summary,
+      onclick: (e) => {
+        vnode.attrs.data.summary = e.target.checked;
       }
     }, ''), ''),
   m('.label', 'Dark Sky Tour Given'),
   m('label.checkbox-inline',
-    m('input[type=checkbox]', {
+    m('input[type="checkbox"]', {
+      checked: vnode.attrs.data.darksky,
       onclick: _ => {
-        vnode.state.darksky_checked = !vnode.state.darksky_checked;
-        vnode.state.data.darksky = vnode.state.darksky_checked;
+        vnode.attrs.data.darksky = !vnode.attrs.data.darksky;
       }
     }, ''), ''),
   m('.label', 'Notes'),
-  m('input.input', {onchange:(e) => vnode.state.data.notes = e.target.value }, ''),
+  m('input.input', {
+    value: vnode.attrs.data.notes,
+    onchange:(e) => vnode.state.data.notes = e.target.value
+  }, ''),
   m('.label', 'Contact'),
-  m('input.input', {onchange:(e) => vnode.state.data.contact = e.target.value }, ''),
+  m('input.input', {
+    value: vnode.attrs.data.contact,
+    onchange:(e) => vnode.state.data.contact = e.target.value
+  }, ''),
 ];
 
 // let addVisitModalBody = (vnode) => [
@@ -148,7 +163,6 @@ let editVisitModalBody = (vnode) => [
 
 let visitCard = {
   oninit:(vnode) => {
-    // vnode.state.edit = {modal:false, type: 'visit', func:getVisit, body:editVisitModalBody};
     vnode.state.open = false;
     vnode.state.values = [];
     vnode.state.openDetails = _ => {
@@ -166,7 +180,6 @@ let visitCard = {
     m('span', {style:'font-weight: bold;'},`End:`),
      m('span', {style:'padding:0 5px;'},`${unixToDate(vnode.attrs.visit.end_date)}`),
     g.groupLookup[vnode.attrs.visit.group_id] ? m('span', {style:"font-weight:bold;"}, g.groupLookup[vnode.attrs.visit.group_id].name) : null,
-    //m('span', vnode.attrs.name),
     vnode.state.open ? m('button.button.is-danger.is-small.is-pulled-right',
       {onclick:_ => {
         deleteData.modal = true;
@@ -175,7 +188,7 @@ let visitCard = {
       }}, 'Delete') : null,
     vnode.state.open ? m('button.button.is-primary.is-small.is-pulled-right',
       {onclick:_ => {
-        //TODO display Edit Modal filled w/ value
+        //Display editVisit Modal w/ visit
         vnode.attrs.editModal.modal = true;
         vnode.attrs.editModal.data = vnode.attrs.visit;
       }}, 'Edit') : null,
@@ -188,8 +201,8 @@ let visitCard = {
 
 export default {
   oninit:(vnode) => {
-    vnode.state.add = {modal:false, type: 'visit', func:addVisit, body:addVisitModalBody };
-    vnode.state.edit = {modal:false, type: 'visit', func:getVisit, body:editVisitModalBody};//must be passed to each visitCard so editModal can be triggered
+    vnode.state.add = {modal:false, type: 'visit', func:addVisit, body: addVisitModalBody};
+    vnode.state.edit = {modal:false, type: 'visit', func:editVisit, body:editVisitModalBody};//must be passed to each visitCard so editModal can be triggered
     if (g.visits.length === 0) getVisits();
     if (g.groups.length === 0) getGroups();
   },
