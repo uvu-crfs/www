1babb9abae3b76af365b0d4c98f5db6b0d1acc20
Jason Mavandi <Jason.Mavandi@emc.com>
2017-04-18 11:35:22 -0600

    Add sensor data updates * Submit on enter * Initial values on home page * button disabled if information missing

--- html/mithril/components/addSensorData.js
+++ html/mithril/components/addSensorData.js
@@ -5,31 +5,40 @@ export default {
     vnode.state = {sensor: Number(vnode.attrs.sensor.id), quantity: 0};
     if (g.visits.length === 0) getVisits();
     if (g.groups.length === 0) getGroups();
+    if (vnode.attrs.homePage) {
+      vnode.state.initialSelectionId = vnode.attrs.visits[0].id;
+      vnode.state.visit_id = vnode.attrs.visits[0].id;
+    }
   },
   oncreate: (vnode) => $(`#sensor_${vnode.attrs.sensor.name}`).select2({ placeholder: "Choose a visit", width : '340px'}),
+  onupdate: (vnode) => $(`#sensor_${vnode.attrs.sensor.name}`).select2({ placeholder: "Choose a visit", width : '340px'}),
   view: function(vnode){
-    return m('form',[
+    return m('form', {
+      onsubmit:function(e){
+        e.preventDefault();
+        if(vnode.state.quantity > 0) addSensorData(vnode.state); },
+    },[
       m('span',  vnode.attrs.sensor.name),
       m('input', {
         type:"number", min:'0', step:"0.1", value:vnode.state.quantity,
         onchange:function(e){ vnode.state.quantity = Number(e.target.value); }
       }, ''),
       m('span', vnode.attrs.sensor.unit),
-      m(`select#sensor_${vnode.attrs.sensor.name}`,{ onchange:function(e){
+      m(`select#sensor_${vnode.attrs.sensor.name}`,{
+        onchange:function(e){
           let value = JSON.parse(e.target.value);
           vnode.state.visit_id = value.id;
           if (!vnode.attrs.homePage) vnode.state.timestamp = value.start_date;
         }},
-        blankFirstOption((vnode.attrs.visits || g.visits).map((v) => m('option',
-          {value:JSON.stringify({id:v.id,start_date:parseInt(v.start_date)+1})},
+        blankFirstOption((vnode.attrs.visits || g.visits).map((v) => m('option',{
+          selected: v.id === vnode.state.initialSelectionId,
+          value:JSON.stringify({id:v.id,start_date:parseInt(v.start_date)+1}),
+        },
         `${unixToDate(v.start_date)} - ${g.groupLookup[v.group_id] ? g.groupLookup[v.group_id].name : ''}`)))
       ),
-      m('button[type="submit"]', {
-        //disabled: "false", TODO figure out how to test if this is working
-        onclick:function(e){
-          e.preventDefault();
-          if(vnode.state.quantity > 0) addSensorData(vnode.state); },
-      }, 'Add Data')
+      m('input[type="submit"][value="Add Data"]', {
+        disabled : (vnode.state.quantity === 0 || typeof(vnode.state.visit_id) === 'undefined')
+      })
     ]);
   }
 };
